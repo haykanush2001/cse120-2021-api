e1b08e70ca4421e73ed2a0b6cd1aea72cf6eab84
haykanush2001 <81684578+haykanush2001@users.noreply.github.com>
2021-05-10 01:34:42 +0400

    transfer from repl.it

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,6 @@
 var myhobby = {
+  "owner": "Haykanush Papoyan",
+  "project": "Singing",
   "fullname" : "",
   "favsinger" : "",
   "whyhersongs" : "",
@@ -63,25 +65,93 @@ function handleOnewordChange() {
 function handleAspectChange() {
   myhobby.aspect=document.getElementById ("aspect").value;
 }
-function SaveData(e){
-  console.log(myhobby)
+function validateFormData() {
+  var isFormValid = true;
+  var keys = Object.keys(myhobby);
+  keys.forEach(key => {
+      if (requiredFields.indexOf(key) > -1 && myhobby[key] == "") { console.log(key, " is a required field, please add a value") 
+      if(document.getElementById(key)) {
+        document.getElementById(key).style.backgroundColor = "red"; 
+        isFormValid = false;
+      }
+    }   
+  })
+  return isFormValid;
+}
+function showTheHobbyData(e){
   e.preventDefault();
-  $.ajax({
-    type: 'POST',
-    url: "https://cse120-2021-api-haykanush.herokuapp.com/data",
-    data: myhobby,
-    cache: false,
-    dataType : 'json',
-    success: function (data) {
-      console.log("success");
-    },
-    error: function (xhr) {
-      console.error("Error in post", xhr);
-    },
-    complete: function () {
-      console.log("Complete");  
-    }
-  });  
+  if(validateFormData() == false) {
+    return;
+  } else {
+    console.log(myhobby);
+    $.ajax({
+      type: 'POST',
+      url: "https://cse120-2021-api-haykanush.herokuapp.com/data",
+      data: myhobby,
+      cache: false,
+      dataType : 'json',
+      success: function (data) {
+        console.log("success");
+      },
+      error: function (xhr) {
+        console.error("Error in post", xhr);
+      },
+      complete: function () {
+        console.log("Complete");  
+      }
+    });
+  }
+}
+ $.ajax({
+      type: 'POST',
+      url: "https://cse120-2021-api-haykanush.herokuapp.com/data/update",
+      data: myhobby,
+      cache: false,
+      dataType : 'json',
+      success: function (data) {
+        console.log("success");
+      },
+      error: function (xhr) {
+        console.error("Error in post", xhr);
+      },
+      complete: function () {
+        console.log("Complete");  
+      }
+    });
+  
+function updateHobby(){
+  var tmp = {
+   "id" : document.getElementById("_id").innerHTML,
+   "fullname" : document.getElementById("fullname").value,
+   "favsinger" : document.getElementById("favsinger").value,
+   "whyhersongs" : document.getElementById("whyhersongs").value,
+   "singingstyle" : document.getElementById("singingstyle").value,
+   "favsong" : document.getElementById("favsong").value,
+   "singingway" : document.getElementById("singingway").value,
+   "singaduet" : document.getElementById("singaduet").value,
+   "training" : document.getElementById("training").value,
+   "audience" : document.getElementById("audience").value,
+   "oneword" : document.getElementById("oneword").value,
+   "aspect" : document.getElementById("aspect").value,
+   
+   
+   }
+   $.ajax({
+        type: 'POST',
+        url: "https://cse120-2021-api-haykanush.herokuapp.com/data/delete",
+        data: tmp,
+        cache: false,
+        dataType : 'json',
+        success: function (data) {
+            console.log("success");
+        },
+        error: function (xhr) {
+            console.error("Error in post", xhr);
+        },
+        complete: function () {
+            console.log("Complete");  
+        }
+    });
 }
 function loadExistingData() {
     var existingData = [];
@@ -106,74 +176,6 @@ function displayData(existingData) {
     document.getElementById("existingData").innerHTML += "<li><i>" + currentBook.fullname + "</li> : <b>" + currentBook.title + "</b></li>";
   }
   document.getElementById("existingData").innerHTML += "</ul>"
-}
-function deleteData(id) {
-
-    var r = confirm("Are you sure you want to delete the item with the following ID? " + id);
-    if (r == true) {
-      
-    } else {
-      return;
-    }
-
-    var tmp = {
-        "id": id
-    }
-
-    $.ajax({
-        type: 'POST',
-        url: "https://cse120-2021-api-haykanush.herokuapp.com/data/delete",
-        data: tmp,
-        cache: false,
-        dataType : 'json',
-        success: function (data) {
-            console.log("success");
-            document.getElementById("div" + id).style.display = "none";
-        },
-        error: function (xhr) {
-            console.error("Error in post", xhr);
-        },
-        complete: function () {
-            console.log("Complete");  
-        }
-    });
-}
-
-function saveData() {
-    var tmp = {
-        "test": "Data"
-    }
-
-    $.ajax({
-        type: 'POST',
-        url: "https://cse120-2021-api-haykanush.herokuapp.com/data",
-        data: tmp,
-        cache: false,
-        dataType : 'json',
-        success: function (data) {
-            console.log("success");
-        },
-        error: function (xhr) {
-            console.error("Error in post", xhr);
-        },
-        complete: function () {
-            console.log("Complete");  
-        }
-    });
-}
-function loadExistingData() {
-  $.ajax({
-    type : "GET",
-    url : "https://cse120-2021-api-haykanush.herokuapp.com/data",
-    dataType : "json",
-    success : function(data) {
-      console.log("success", data);
-        displayData(data.data);
-    },
-    error : function(data) {
-        console.log("Error")
-    }
-  });
 }
 function displayData(data) {
   document.getElementById("dataContainer").innerHTML = "";
@@ -202,7 +204,7 @@ function displayData(data) {
     
   var span1 = document.createElement("span");
       span1.className = "undefined";
-      span1.innerHTML = "N/A";
+      span1.innerHTML = "---";
       span.appendChild(span1)
             }
       item.appendChild(span);
@@ -219,8 +221,8 @@ function displayData(data) {
     item.appendChild(button);
     document.getElementById("dataContainer").appendChild(item);
 })
-
 }
+var loadedData = [];
 function loadEditItem() {
   localStorage = window.localStorage;
   editItem = JSON.parse(localStorage.getItem("editItem"));
@@ -243,7 +245,25 @@ function loadEditItem() {
   document.getElementById("origpdate").value = editItem["origpdate"];
   document.getElementById("agerestriction").value = editItem["agerestriction"];
   document.getElementById("genre").value = editItem["genre"];
-}    
+}  
+function loadHobbyEditItem() {
+  localStorage = window.localStorage;
+  editItem = JSON.parse(localStorage.getItem("editItem"));
+  console.log(editItem);
+  document.getElementById("_id").innerHTML = editItem["_id"];
+  document.getElementById("fullname").value = editItem["fullname"];
+  document.getElementById("favsinger").value = editItem["favsinger"];   
+  document.getElementById("whyhersongs").value = editItem["whyhersongs"];   
+  document.getElementById("singingstyle").value = editItem["singingstyle"];
+  document.getElementById("favsong").value = editItem["favsong"];
+  document.getElementById("singingway").value = editItem["singingway"];
+  document.getElementById("singaduet").value = editItem["singaduet"];
+  document.getElementById("training").value = editItem["training"];
+  document.getElementById("audience").value = editItem["audience"];
+  document.getElementById("oneword").value = editItem["oneword"];
+  document.getElementById("aspect").value = editItem["aspect"];
+  
+} 
 function editData(id) {
   var tmp = id.split("edit_");
   var item_id = tmp[1];
@@ -253,10 +273,14 @@ function editData(id) {
       console.log(item); 
       localStorage = window.localStorage;
       localStorage.setItem('editItem', JSON.stringify(item));
-      document.location  = "form.html"; 
-    }
-  })
-} 
+      if (item.project == "Singing") {
+              document.location  = "hobby.html";
+            } else {
+              document.location  = "book.html"; 
+            }
+        }
+    })
+}
 function deleteData(id) {
 
   var r = confirm("Are you sure you want to delete the item with the following ID? " + id);
@@ -284,7 +308,7 @@ function deleteData(id) {
       console.log("Complete");  
     }
   });
-}    
+}  
 function saveData() {
     var tmp = {
         "test": "Data"
@@ -339,7 +363,6 @@ function loadExistingData() {
   });
 }
 function displayData(data, containerDivName) {
-  loadedData = data;
   document.getElementById(containerDivName).innerHTML = "";
   data.forEach(elem => {
     var item = document.createElement("div");
@@ -401,4 +424,177 @@ function toggleOtherData() {
     otherData.style.display = "block";
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var myfavbook = {
+  "owner": "Haykanush Papoyan",
+  "project": "Book",
+  "fullname": "",
+  "author": "",
+  "colour": "",
+  "covertype": "",
+  "othercovervalue": "",
+  "othercovertype": "",
+  "numberofpages": "",
+  "price": "",
+  "currency": "",
+  "language": "",
+  "otherlanguage": "",
+  "olanguage": "",
+  "otherolanguage": "",
+  "edition": "",
+  "dimensions": "",
+  "publisher": "",
+  "pdate": "",
+  "origpdate": "",
+  "genre": "",
+  "agerestriction": "",
+}
+
+function handleFullnameChange() {
+  myfavbook.fullname= document.getElementById("fullname"). value;
+}
+function handleAuthorChange() {
+  myfavbook.author= document.getElementById("author"). value;
+}
+
+function handleColourChange() {
+  myfavbook.colour= document.getElementById("colour"). value;
+}
+
+function handleCovertypechange(e){ 
+  myfavbook.covertype=e.target.value;
+  if(myfavbook.covertype!="other") {
+    myfavbook.othercovervalue="";
+    document.getElementById("othercovertype").style.display="none";
+  } 
+  else{
+    document.getElementById("othercovertype").style.display="block";
+  }
+}
+function handleCovermaterialchange() {
+  if (myfavbook.covertype == "other") {
+    myfavbook.othercovervalue = document.getElementById("othercovertype").value;
+    document.getElementById("othercovertype").style.display="block";
+  }
+}
+function handlenumberofpagesChange() {
+  myfavbook.numberofpages= document.getElementById("numberofpages"). value;
+}
+function handlepriceChange() {
+  myfavbook.price= document.getElementById("price"). value;
+}
+function handlecurrencyChange() {
+  myfavbook.currency= document.getElementById("currency"). value;
+}
+
+function handleLanguageChange(e){ 
+  myfavbook.language=e.target.value;
+  if(myfavbook.language!="other") {
+    myfavbook.otherlanguage="";
+    document.getElementById("otherlanguage").style.display="none";
+  } 
+  else{
+    document.getElementById("otherlanguage").style.display="block";
+  }
+}
+function handleLanguagevalue() {
+  if (myfavbook.language == "other") {
+    myfavbook.otherlanguage=document.getElementById("otherlanguage").value;
+    document.getElementById("otherlanguage").style.display="block";
+  }
+}
+
+function handleOLanguageChange(e){ 
+  myfavbook.olanguage=e.target.value;
+  if(myfavbook.olanguage!="other") {
+    myfavbook.otherolanguage="";
+    document.getElementById("otherolanguage").style.display="none";
+  } 
+  else{
+    document.getElementById("otherolanguage").style.display="block";
+  }
+}
+function handleOLanguagevalue() {
+  if (myfavbook.olanguage == "other") {
+    myfavbook.otherolanguage=document.getElementById("otherolanguage").value;
+    document.getElementById("otherolanguage").style.display="block";
+  }
+}
+function handleeditionChange() {
+  myfavbook.edition= document.getElementById("edition"). value;
+}
+function handledimensionsChange() {
+  myfavbook.dimensions= document.getElementById("dimensions"). value;
+}
+function handlepublisherChange() {
+  myfavbook.publisher= document.getElementById("publisher"). value;
+}
+function handlepdateChange() {
+  myfavbook.pdate= document.getElementById("pdate"). value;
+}
+function handleorigpdateChange() {
+  myfavbook.origpdate= document.getElementById("origpdate"). value;
+}
+function handlegenreChange() {
+  myfavbook.genre= document.getElementById("genre"). value;
+}
+function handleagerestrictionChange() {
+  myfavbook.agerestriction= document.getElementById("agerestriction"). value;
+}
+
+function showTheBookData(e){
+  e.preventDefault();
+  console.log(myfavebook);
+  $.ajax({
+    type: 'POST',
+    url: "https://cse120-2021-api-haykanush.herokuapp.com/data",
+    data: myfavebook,
+    cache: false,
+    dataType : 'json',
+    success: function (data) {
+      console.log("success");
+    },
+    error: function (xhr) {
+      console.error("Error in post", xhr);
+    },
+    complete: function () {
+      console.log("Complete");  
+    }
+  });
+}
+function updateBook(){
+  var tmp = {
+   "id" : document.getElementById("_id").innerHTML,
+   "fullname" : document.getElementById("fullname").value,
+   "title": document.getElementById("title").value,
+   "author": document.getElementById("author").value,
+   "covertype": document.getElementById("covertype").value,
+   "numberofpages": document.getElementById("numberofpages").value,
+   "price": document.getElementById("price").value,
+   "currency": document.getElementById("currency").value,
+   "language": document.getElementById("language").value,
+   "olanguage": document.getElementById("olanguage").value,
+   "edition": document.getElementById("edition").value,
+   "dimensions": document.getElementById("dimensions").value,
+   "publisher": document.getElementById("publisher").value,
+   "pdate": document.getElementById("pdate").value,
+   "origpdate": document.getElementById("origpdate").value,
+   "genre": document.getElementById("genre").value,
+   "agerestriction": document.getElementById("agerestriction").value,
+  
+   }
+
 
