8433b25368ed6a56e6ad9ce1c304fcf9e37527ea
haykanush2001 <81684578+haykanush2001@users.noreply.github.com>
2021-05-14 01:21:09 +0400

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -240,22 +240,25 @@ function loadHobbyEditItem() {
   document.getElementById("aspect").value = editItem["aspect"];
   
 } 
-function editData(id) {
-  var tmp = id.split("edit_");
-  var item_id = tmp[1];
 
-  loadedData.forEach(item => {
-    if (item._id == item_id) {
-      console.log(item); 
-      localStorage = window.localStorage;
-      localStorage.setItem('editItem', JSON.stringify(item));
-      if (item.project == "Singing") {
-              document.location  = "hobby.html";
-            } else {
-              document.location  = "book.html"; 
-            }
+function editData(e, id){
+  e.stopImmediatePropagation();
+ var tmp = id.split("edit_");
+ var item_id = tmp[1];
+ console.log(item_id);
+
+loadedData.forEach(item => {
+    if (item._id == item_id && item["owner"] == "Haykanush Papoyan") {
+        console.log(item); 
+        localStorage = window.localStorage;
+        localStorage.setItem('editItem', JSON.stringify(item));
+        if (item["project"] == "Singing") {
+          document.location  = "hobby.html"; 
+        } else {
+          document.location  = "book.html"; 
         }
-    })
+    }
+  })
 }
 function deleteData(id) {
 
